a084fe7873e601dccf1dbde94474f4ba9827fd05
Habib Ahmad <habibahmed001@gmail.com>
2020-03-31 15:10:35 +0500

    Push all new files 03312020

---
+++ database/migrations/2020_03_27_102730_Table_Mexam_With_User.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TableMexamWithUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tablemexamwithuser', function (Blueprint $table) {
+            $table->increments('id',100);
+            $table->integer('mexam_id')->nullable();
+            $table->integer('user_id')->nullable();
+
+            $table->foreign('mexam_id')
+                ->references('id')
+                ->on('tablemexam')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tablemexamwithuser');
+    }
+}
